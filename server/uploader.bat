ee8d5f1f7ce2318c30e9cdaca740d3e90781b39b
SayCV <sssxyc@163.com>
2013-02-06 21:25:16 +0800

    update user info.

--- server/uploader.bat
+++ server/uploader.bat
@@ -1,1 +1,11 @@
+@echo off
+echo Copyright 2011 SayCV Inc. All rights reserved.
+echo Use of this source code is governed by a BSD-style
+echo license that can be found in the LICENSE file.
+echo.
+echo 2013 @ SayCV.Xiao
+echo.
+
+cd /d %~dp0
 @"%~dp0..\local\python27.exe" uploader.zip
+pause
